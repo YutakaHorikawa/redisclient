6695ed681684a8c5b4f3ddf80c4b08b95d4b5c86
YutakaHorikawa <gozutakemaru@gmail.com>
2014-05-19 01:18:12 +0900

    ロック

--- sider.py
+++ sider.py
@@ -10,6 +10,7 @@ class RedisData(AttributeRedis):
 class SettingsPanel(wx.Panel):
     def __init__(self, parent, id):
         wx.Panel.__init__(self, parent, id, style=wx.BORDER_SUNKEN)
+        self._lock = False
         self._parent = parent
         self._redis_connect_button = wx.Button(self, -1, u'接続', (360, 15))
 
@@ -25,6 +26,10 @@ class SettingsPanel(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.redis_connect, id=self._redis_connect_button.GetId())
 
     def redis_connect(self, event):
+        if self._lock:
+            wx.MessageBox('Please wait! It is generated in the data grid.', 'Warning')
+            return
+
         #TODO バリデーション
         if self._port_box.IsEmpty() or self._host_name_box.IsEmpty():
             wx.MessageBox('Host Name or Port Number is Empty.', 'Error')
@@ -35,6 +40,9 @@ class SettingsPanel(wx.Panel):
 
         redis = RedisData(host_name, int(port_number), class_name=False)
         self._parent.GetParent().redis_data_panel.generate_redis_data_grid(redis)
+
+    def update_lock_flag(self, value):
+        self._lock = value
 
 class RedisDataGrid(wx.grid.Grid):
 
@@ -51,7 +59,7 @@ class RedisDataGrid(wx.grid.Grid):
         self.SetCellValue(0, 1, "Data Type")
         self.SetCellValue(0, 2, "Value")
 
-    def generate_redis_data_grid(self, redis):
+    def generate_redis_data_grid(self, redis, callback=None):
         self._redis = redis
         self._data = self._redis.get_all(key=True)
 
@@ -63,13 +71,15 @@ class RedisDataGrid(wx.grid.Grid):
                 self.SetCellValue(self._last_line, 0, key)
                 self.SetCellValue(self._last_line, 1, data_type)
                 self.SetCellValue(self._last_line, 2, value)
-
             except wx._core.PyAssertionError:
                 print redis_data
                 pass
             else:
                 self._update_last_line()
                 self.AppendRows(self._last_line)
+
+        if callback:
+            callback()
 
     def _update_last_line(self):
         self._last_line += 1
@@ -79,6 +89,7 @@ class RedisDataGrid(wx.grid.Grid):
 
 class RedisDataPanel(wx.Panel):
     def __init__(self, parent, id):
+        self._parent = parent
         wx.Panel.__init__(self, parent, id, style=wx.BORDER_SUNKEN)
         grid = wx.GridBagSizer(hgap=5, vgap=5)
         self._rgrid = RedisDataGrid(self, -1)
@@ -86,7 +97,8 @@ class RedisDataPanel(wx.Panel):
         self.SetSizerAndFit(grid)
 
     def generate_redis_data_grid(self, redis):
-        self._rgrid.generate_redis_data_grid(redis)
+        self._parent.GetParent().settings_panel.update_lock_flag(True)
+        self._rgrid.generate_redis_data_grid(redis, lambda: self._parent.GetParent().settings_panel.update_lock_flag(False))
 
 class Sider(wx.Frame):
     def __init__(self, parent, id, title):
@@ -94,11 +106,11 @@ class Sider(wx.Frame):
         self._redis = None
 
         panel = wx.Panel(self, -1)
-        self._settings_panel = SettingsPanel(panel, -1)
+        self.settings_panel = SettingsPanel(panel, -1)
         self.redis_data_panel = RedisDataPanel(panel, -1)
 
         layout = wx.BoxSizer(wx.VERTICAL)
-        layout.Add(self._settings_panel, 1, wx.EXPAND | wx.ALL, 5)
+        layout.Add(self.settings_panel, 1, wx.EXPAND | wx.ALL, 5)
         layout.Add(self.redis_data_panel, 1, wx.EXPAND | wx.ALL, 5)
 
         panel.SetSizer(layout)
